46d1f1e271acb8ba9c0d09956fc2bbecf081d671
YuXianLin <410035042@gms.ndhu.edu.tw>
2018-07-19 20:37:25 +0800

    8.0 before

--- xianCore.lua
+++ xianCore.lua
@@ -1,21 +1,27 @@
 xianCore = {};
 
+local stillList = {
+  [64843] = 8,
+  [64844] = 8,
+};
+local stillSpelling = false;
+
 local function say(talks, idx, player, linkStr, target)
   local ch = "YELL";
   if IsInInstance() then
-    -- local name, itype, difficultyIndex, difficultyName = GetInstanceInfo();
     if HasLFGRestrictions() then
       ch = "INSTANCE_CHAT";
     elseif IsInRaid() then
       if UnitIsGroupAssistant("player") or UnitIsGroupLeader("player") then
         ch = "RAID_WARNING";
       else
-        ch = "RIAD";
+        ch = "RAID";
       end
-    else
+    elseif UnitInParty("player") then
       ch = "PARTY";
     end
   end
+  print(ch);
   local text = talks[idx]["text"];
   local t = talks[idx]["int"];
   text = string.gsub(text, "%%player", player);
@@ -28,24 +34,53 @@ local function say(talks, idx, player, linkStr, target)
     C_Timer.After(t, function()
       say(talks, idx, player, linkStr, target);
     end);
+  else
+    stillSpelling = false;
   end
 end
 
+local function setStillSpelling(t)
+  stillSpelling = true;
+  C_Timer.After(t, function()
+    stillSpelling = false;
+  end);
+end
+
 local function unitSpellCastS(...)
-  local status, caster, spellName, spellRank, spellIdCounter, spellId = ...;
+  local status, caster, _, spellId = ...;
+  local spellName = GetSpellInfo(spellId);
+  if testMode == true then
+    print(caster, spellId, spellName);
+  end
   local target = UnitName("target") or "";
   local player = UnitName("player");
   local linkStr = "|cff71d5ff|Hspell:"..spellId.."|h["..spellName.."]|h|r";
   if caster == "player" then
     if status == "SUCCEEDED" then
       local talks = xianSetting.SUCCESS[spellName];
+      if talks == nil then
+        talks = xianSetting.SUCCESS[spellId];
+      end
       if talks ~= nil and talks[1] ~= nil then
-        say(talks, 1, player, linkStr, target);
+        if stillList[spellId] == nil or stillSpelling ~= true then
+          if stillList[spellId] ~= nil then
+            setStillSpelling(stillList[spellId]);
+          end
+          say(talks, 1, player, linkStr, target);
+        end
       end
     elseif status == "START" then
       local talks = xianSetting.SEND[spellName];
+      if talks == nil then
+        talks = xianSetting.SEND[spellId];
+      end
       if talks ~= nil and talks[1] ~= nil then
-        say(talks, 1, player, linkStr, target);
+        if stillList[spellId] == nil or stillSpelling ~= true then
+          if stillList[spellId] ~= nil then
+            setStillSpelling(stillList[spellId]);
+          end
+          say(talks, 1, player, linkStr, target);
+        end
       end
     end
   end
@@ -56,16 +91,23 @@ function xianCore.create(theFrame)
   -- register events
   xianCore.frame:RegisterEvent("UNIT_SPELLCAST_START");
   xianCore.frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
-  --xianCore.frame:RegisterEvent("COMBAT_LOG_EVENT");
+  xianCore.frame:RegisterEvent("COMBAT_LOG_EVENT");
 
   xianCore.frame:SetScript("OnEvent", function(...)
-    local arg1, event = ...;
+    local _, event = ...;
+    if testMode then
+      print(...); -- test mode
+    end
     if event == "UNIT_SPELLCAST_SUCCEEDED" then
       unitSpellCastS("SUCCEEDED", select(3, ...));
     elseif event == "UNIT_SPELLCAST_START" then
-      unitSpellCastS("START", select(3, ...));
-    elseif event == "COMBAT_LOG_EVENT" then
-      SendChatMessage("來玩吧", "yell");
+      -- unitSpellCastS("START", select(3, ...));
+    elseif event == "COMBAT_LOG_EVENT" and testMode then
+      local _, _, _, event, _, _, _, _, _, _, _, _, auraType, _, skill = ...;
+      if skill == "惡魔炸彈" then
+        -- print(skill); -- test mode
+      end
+      -- SendChatMessage("來玩吧", "yell");
     end
   end);
 end
--- xianSetting.lua
+++ xianSetting.lua
@@ -127,6 +127,344 @@ xianSetting = {
         ["int"] = 0,
         ["text"] = "%skill 歡樂糖果機 已佈署, 歡迎取用~",
       }
+    },
+    ["時間扭曲"] = {
+      {
+        ["int"] = 0,
+        ["text"] = "%skill！！",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間40秒"
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間39秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "跟上我的tempo 大家唱起來～",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間38秒"
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間37秒"
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間36秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "- Erlkönig -",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間35秒"
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間34秒"
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間33秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Wer reitet so spät durch Nacht und Wind?",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間32秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Es ist der Vater mit seinem Kind;",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間31秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Er hat den Knaben wohl in dem Arm,",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間30秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Er fasst ihn sicher, er hält ihn warm.",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間29秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Mein Sohn, was birgst du so bang dein Gesicht? –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間28秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Siehst, Vater, du den Erlkönig nicht?",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間27秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Den Erlenkönig mit Kron’ und Schweif? –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間26秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Mein Sohn, es ist ein Nebelstreif. –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間25秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "„Du liebes Kind, komm, geh mit mir!",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間24秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Gar schöne Spiele spiel’ ich mit dir;",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間23秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Manch’ bunte Blumen sind an dem Strand,",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間22秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Meine Mutter hat manch gülden Gewand.“ –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間21秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Mein Vater, mein Vater, und hörest du nicht,",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間20秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Was Erlenkönig mir leise verspricht? –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間19秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Sei ruhig, bleibe ruhig, mein Kind;",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間18秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "In dürren Blättern säuselt der Wind. –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間17秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "„Willst, feiner Knabe, du mit mir gehn?",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間16秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Meine Töchter sollen dich warten schön;",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間15秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Meine Töchter führen den nächtlichen Reihn",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間14秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Und wiegen und tanzen und singen dich ein.“ –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間13秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Mein Vater, mein Vater, und siehst du nicht dort",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間12秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Erlkönigs Töchter am düstern Ort? –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間11秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Mein Sohn, mein Sohn, ich seh’ es genau:",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間10秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Es scheinen die alten Weiden so grau. –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間9秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "„Ich liebe dich, mich reizt deine schöne Gestalt;",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間8秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Und bist du nicht willig, so brauch’ ich Gewalt.“ –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間7秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Mein Vater, mein Vater, jetzt faßt er mich an!",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間6秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Erlkönig hat mir ein Leids getan! –",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間5秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Dem Vater grauset’s; er reitet geschwind,",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間4秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Er hält in Armen das ächzende Kind,",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間3秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "Erreicht den Hof mit Mühe und Not;",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間2秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "In seinen Armen das Kind war tot.",
+      },
+      {
+        ["int"] = 1,
+        ["text"] = "buff剩餘時間1秒"
+      },
+      {
+        ["int"] = 0,
+        ["text"] = "效果結束, 感謝大家的努力～",
+      },
+    },
+    ["神聖禮頌"] = {
+      {
+        ["int"] = 4,
+        ["text"] = "cast %skill !"
+      },
+      {
+        ["int"] = 4,
+        ["text"] = "casting %skill . . ."
+      },
+      {
+        ["int"] = 100,
+        ["text"] = "cast end"
+      }
+    },
+    [642] = {
+      {
+        ["int"] = 1,
+        ["text"] = "%player 開啟無敵蛋殼!! %skill"
+      }
+    },
+    [19750] = {
+      {
+        ["int"] = 1,
+        ["text"] = "為 %target 施放 %skill~"
+      }
+    },
+    [1022] = {
+      {
+        ["int"] = 1,
+        ["text"] = "為 %target 施放 %skill 請好心的補大大們救救牠"
+      }
     }
-  },
+  }
 };
